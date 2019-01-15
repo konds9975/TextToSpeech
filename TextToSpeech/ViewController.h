4664319c3933d8227bf45e30079d30ad1736b7f5
Mac Admin <macadmin@Macs-Mac-mini.local>
2019-01-15 16:38:14 +0530

    init

--- TextToSpeech/ViewController.h
+++ TextToSpeech/ViewController.h
@@ -6,10 +6,23 @@
 //  Copyright © 2019 Mac Admin. All rights reserved.
 //
 
+//#import <UIKit/UIKit.h>
+//#import <AVFoundation/AVFoundation.h>
+//#import <QuartzCore/QuartzCore.h>
+//@interface ViewController : UIViewController
+//
+//
+//@end
+//
+
+
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <AVSpeechSynthesizerDelegate>
 
+@property (weak, nonatomic) IBOutlet UIButton *playPauseButton;
+@property (weak, nonatomic) IBOutlet UITextView *textToSpeak;
+@property (strong, nonatomic) AVSpeechSynthesizer *synthesizer;
 
 @end
-
--- TextToSpeech/ViewController.m
+++ TextToSpeech/ViewController.m
@@ -6,6 +6,36 @@
 //  Copyright © 2019 Mac Admin. All rights reserved.
 //
 
+//#import "ViewController.h"
+//
+//@interface ViewController ()
+//
+//@end
+//
+//@implementation ViewController
+//
+//- (void)viewDidLoad {
+//    [super viewDidLoad];
+//
+//
+//    NSString *str = @"Hello friend, how are you?";
+//
+//    AVSpeechSynthesizer *synthesizer = [[AVSpeechSynthesizer alloc]init];
+//    AVSpeechUtterance *speechutt = [AVSpeechUtterance speechUtteranceWithString:str];
+//    speechutt.volume=100.0f;
+//    speechutt.rate=50.0f;
+//
+//    speechutt.pitchMultiplier=50.0f;
+//    [speechutt setRate:0.4f];
+//    speechutt.voice = [AVSpeechSynthesisVoice voiceWithLanguage:@"en-us"];
+//    [synthesizer speakUtterance:speechutt];
+//
+//}
+//
+//
+//
+//@end
+
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -14,10 +44,46 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+BOOL speechPaused = 0;
+
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.synthesizer = [[AVSpeechSynthesizer alloc] init];
+    speechPaused = NO;
+    self.synthesizer.delegate = self;
 }
 
+- (IBAction)playPauseButtonPressed:(UIButton *)sender {
+    [self.textToSpeak resignFirstResponder];
+    if (speechPaused == NO) {
+        [self.playPauseButton setTitle:@"Pause" forState:UIControlStateNormal];
+        [self.synthesizer continueSpeaking];
+        speechPaused = YES;
+    } else {
+        [self.playPauseButton setTitle:@"Play" forState:UIControlStateNormal];
+        speechPaused = NO;
+        [self.synthesizer pauseSpeakingAtBoundary:AVSpeechBoundaryImmediate];
+    }
+    if (self.synthesizer.speaking == NO) {
+        AVSpeechUtterance *utterance = [[AVSpeechUtterance alloc] initWithString:self.textToSpeak.text];
+        //utterance.rate = AVSpeechUtteranceMinimumSpeechRate;
+        utterance.voice = [AVSpeechSynthesisVoice voiceWithLanguage:@"en-us"];
+        [self.synthesizer speakUtterance:utterance];
+    }
+}
+
+-(void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer didFinishSpeechUtterance:(AVSpeechUtterance *)utterance {
+    [self.playPauseButton setTitle:@"Play" forState:UIControlStateNormal];
+    speechPaused = NO;
+    NSLog(@"Playback finished");
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
 
 @end
